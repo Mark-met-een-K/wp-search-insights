e93b9dc5781b8041dc726b1e7afecc43e844b796
Mark <markie_wolters77@hotmail.com>
2019-12-02 21:55:39 +0100

    shepherd met logo

--- assets/js/wpsi-tour.js
+++ assets/js/wpsi-tour.js
@@ -15,8 +15,8 @@ tour = new Shepherd.Tour({
 
 tour.addStep('wpsi-step-1', {
     title: 'We are recording your searches!',
-    text: pluginUrl,
-    // text: '<span>pluginUrl,/"assets/images/logo.png"</span><b>Welcome to Search Insights</b>, get insigth in what your visitors are looking for!',
+    // text: pluginUrl,
+    text: '<span><img src="'+pluginUrl+'assets/images/logo.png"></span><b>Welcome to Search Insights</b>, get insigth in what your visitors are looking for!',
     attachTo: '.search-insights-most-popular bottom',
     classes: 'shepherd-theme-arrows',
     buttons: [
