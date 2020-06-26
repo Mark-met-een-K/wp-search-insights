896a59f998310c4fbfe6120de05ce0eb2a08f573
Rogier Lankhorst <rogier@really-simple-plugins.com>
2020-06-26 21:57:09 +0200

    move integrations logic

--- integrations/integrations.php
+++ integrations/integrations.php
@@ -1,5 +1,10 @@
 <?php
 defined('ABSPATH') or die("you do not have access to this page!");
 
-require_once('bbpress.php');
-require_once('toolset.php');
+if (class_exists( 'bbPress' )) {
+	require_once( 'bbpress.php' );
+}
+
+if (defined('toolset_constant_here') ){
+	require_once('toolset.php');
+}
--- integrations/toolset.php
+++ integrations/toolset.php
@@ -1,8 +1,9 @@
 <?php
 add_action('admin_init', 'wpsi_maybe_set_custom_search_parameter');
 function wpsi_maybe_set_custom_search_parameter(){
-	if (defined('toolset_constant_here')) {
+	if (!get_option('wpsi_toolset_configured') ) {
 		update_option('wpsi_custom_search_parameter', 'wpv_post_search');
+		update_option('wpsi_toolset_configured', true);
 	}
 }
 
