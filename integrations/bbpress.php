88d07a08e6009ace09b7d85dffaf1b6efc8be506
Mark-met-een-K <markie_wolters77@hotmail.com>
2023-01-07 11:14:25 +0100

    fixed BBPress integration

--- integrations/bbpress.php
+++ integrations/bbpress.php
@@ -1,22 +1,27 @@
 <?php
 defined('ABSPATH') or die("you do not have access to this page!");
 
-
 /**
- * Process BBPRess searches
+ * Process BBPress searches
  * @param $posts
  * @param $query
  *
  * @return mixed
  */
 
-function wpsi_track_bbpress($posts, $query){
-	$result_count = empty($posts) ? 0 : count($posts);
+function wpsi_track_bbpress( $posts, $query ) {
+
+    if ( is_array( $query->posts ) ) {
+        $result_count = count( $query->posts );
+    } else {
+        $result_count = 0;
+    }
 
 	if (isset($query->query_vars['s'])){
 		$search_terms = $query->query_vars['s'];
 		WPSI::$search->process_search_term( $search_terms , $result_count, 'bbpress');
 	}
+
 	return $posts;
 }
 add_filter( 'bbp_has_search_results', 'wpsi_track_bbpress', 10, 2);
